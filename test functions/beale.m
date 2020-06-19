dbb31592517dc8b7aecd560efc40ab531b386329
amalshaji <amalshajid@gmail.com>
2020-06-19 13:05:09 +0530

    removed test functions

--- test functions/beale.m
+++
@@ -1,8 +0,0 @@
-function [global_minima] = beale(inputs)
-%BEALE Summary of this function goes here
-%   Detailed explanation goes here
-    x = inputs(1);
-    y = inputs(2);
-    global_minima = (1.5 - x + x*y)^2 + (2.25 - x + x*y*y)^2 + (2.625 - x + x*y*y*y)^2;
-end
-
--- test functions/booth.m
+++
@@ -1,8 +0,0 @@
-function [global_minima] = booth(inputs)
-%BOOTH Summary of this function goes here
-%   Detailed explanation goes here
-    x = inputs(1);
-    y = inputs(2);
-    global_minima = (x + 2*y -7)^2 + (2*x + y - 5)^2;
-end
-
